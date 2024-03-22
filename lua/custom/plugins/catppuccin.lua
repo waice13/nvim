05ee9148c160e25f87e326e417c3822ba8acf511
waice13 <warut.k@datawow.io>
2024-03-22 18:14:30 +0700

    init repository

---
+++ lua/custom/plugins/catppuccin.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+        require("catppuccin").setup {
+        color_overrides = {
+          all = {
+            text = "#ffffff",
+          },
+          latte = {},
+          frappe = {},
+          macchiato = {},
+          mocha = {
+            base = "#000000",
+            mantle = "#000000",
+            crust = "#000000",
+          },
+        }
+      }
+      vim.cmd.colorscheme "catppuccin-mocha"
+    end
+  }
+}
---
+++ lua/custom/plugins/neo-tree.lua
@@ -0,0 +1,16 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require('neo-tree').setup {}
+		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+		vim.keymap.set("n", "<leader>e", ":Neotree toggle left<CR>", {silent = true, noremap = true})
+		vim.keymap.set('n', '<leader>bd', ':bd<CR>', {noremap = true, silent = true, desc = "Close current buffer"})
+		vim.keymap.set('n', '<leader>bo', ':%bd|e#|bd#<CR>', {noremap = true, silent = true, desc = "Close all other buffers"})
+	end,
+}
