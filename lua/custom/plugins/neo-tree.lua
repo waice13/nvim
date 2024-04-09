23e0b2dbc4f50a26d877f3feba7f6c4a3ddf0a59
waice13 <warut.k@datawow.io>
2024-04-09 15:56:07 +0700

    update neo-tree config

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -7,7 +7,11 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
-		require('neo-tree').setup {}
+		require('neo-tree').setup {
+		    filesystem = {
+			follow_current_file = true, -- Enable following the current file
+		    },
+		}
 		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
 		vim.keymap.set("n", "<leader>e", ":Neotree toggle left<CR>", {silent = true, noremap = true})
 		vim.keymap.set('n', '<leader>bd', ':bd<CR>', {noremap = true, silent = true, desc = "Close current buffer"})
